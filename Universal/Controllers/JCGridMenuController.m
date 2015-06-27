e2a3d17a7aabd31bb61cd336967d2665de124efd
Peter Hedlund <peter@peterandlinda.com>
2015-06-27 16:03:49 -0700

    Remove obsolete code.

--- Universal/Controllers/JCGridMenuController.m
+++ Universal/Controllers/JCGridMenuController.m
@@ -44,12 +44,6 @@
 	// Do any additional setup after loading the view.
 }
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return YES; //(interfaceOrientation == UIInterfaceOrientationPortrait);
